37e2dc1cf25787f42863a2d01119b46aea81e24a
Hanish Rishen <hanishrishen@gmail.com>
2024-07-12 10:37:31 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+      dirs: ['pages', 'components', 'public'],
+    },
+    rules: {
+      'react/no-unescaped-entities': 'off',
+    },
+  };
+  
+  export default nextConfig;
